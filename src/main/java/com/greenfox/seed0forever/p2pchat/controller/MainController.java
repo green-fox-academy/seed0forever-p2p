dbf23bd6576b933445c1952d135909bc6d4f5196
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-17 14:48:14 +0200

    add reading 'Client Id' 'Peer address' from environmental variables

--- src/main/java/com/greenfox/seed0forever/p2pchat/controller/MainController.java
+++ src/main/java/com/greenfox/seed0forever/p2pchat/controller/MainController.java
@@ -10,6 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class MainController {
 
+  // GitHub username of app developer
+  // read from environment variable CHAT_APP_UNIQUE_ID
+  private String chatAppUniqueId;
+  // Heroku URL address of an other chat application
+  // read from environment variable CHAT_APP_PEER_ADDRESSS
+  private String chatAppPeerAddress;
+
+  public MainController() {
+    this.chatAppUniqueId = System.getenv("CHAT_APP_UNIQUE_ID");
+    this.chatAppPeerAddress = System.getenv("CHAT_APP_PEER_ADDRESSS");
+  }
+
   @RequestMapping("")
   public String showMainPage(Model model) {
     String currentLogLevel = System.getenv("CHAT_APP_LOGLEVEL");
@@ -24,6 +36,8 @@ public class MainController {
     }
 
     model.addAttribute("developedBy", "seed0forever");
+    model.addAttribute("chatAppUniqueId", chatAppUniqueId);
+    model.addAttribute("chatAppPeerAddress", chatAppPeerAddress);
     return "index";
   }
 
