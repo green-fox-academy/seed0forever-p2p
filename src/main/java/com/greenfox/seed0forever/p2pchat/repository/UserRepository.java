9160ac67b9afdfb81224a81db2cd224ae7e46ba0
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-18 17:01:07 +0200

    polish: add @Override annotation where relevant

--- src/main/java/com/greenfox/seed0forever/p2pchat/repository/UserRepository.java
+++ src/main/java/com/greenfox/seed0forever/p2pchat/repository/UserRepository.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+  @Override
   List<User> findAll();
 
   User findFirstByUsernameIgnoreCase(String username);
+
+  @Override
+    User save(User user);
 }
