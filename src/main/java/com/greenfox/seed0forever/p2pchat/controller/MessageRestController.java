c555203f68774b0679b64c91cdc9ca72220cd9a2
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-19 10:11:44 +0200

    RestController: add logging, move CrossOrigin

--- src/main/java/com/greenfox/seed0forever/p2pchat/controller/MessageRestController.java
+++ src/main/java/com/greenfox/seed0forever/p2pchat/controller/MessageRestController.java
@@ -3,6 +3,7 @@ package com.greenfox.seed0forever.p2pchat.controller;
 import com.greenfox.seed0forever.p2pchat.model.rest.ChatRestMessage;
 import com.greenfox.seed0forever.p2pchat.model.rest.OkRestMessage;
 import com.greenfox.seed0forever.p2pchat.model.rest.RestMessageObject;
+import com.greenfox.seed0forever.p2pchat.service.LogService;
 import com.greenfox.seed0forever.p2pchat.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,19 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/message")
+@CrossOrigin("*")
 public class MessageRestController {
 
+  private LogService logService;
   private MessageService messageService;
 
   @Autowired
-  public MessageRestController(
+  public MessageRestController(LogService logService,
           MessageService messageService) {
+    this.logService = logService;
     this.messageService = messageService;
   }
 
-  @CrossOrigin("*")
   @PostMapping("/receive")
   public RestMessageObject receiveMessage(@RequestBody ChatRestMessage receivedRestMessage) {
+    logService.printLogIfNeeded(
+            "/api/message/receive",
+            "POST",
+            "INFO",
+            receivedRestMessage.toString());
+
     messageService.save(receivedRestMessage.getMessage());
     return new OkRestMessage("ok");
   }
